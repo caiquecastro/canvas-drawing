c4f97c4d337a88f138a0653d080558fecf0dbd4b
Caique de Castro <caique.castro@venturus.org.br>
2023-04-04 11:09:21 -0300

    style: format netlify function script

--- netlify/functions/crop-image/crop-image.ts
+++ netlify/functions/crop-image/crop-image.ts
@@ -1,11 +1,11 @@
-import { Handler } from '@netlify/functions'
-import sharp from 'sharp';
-import fetch from 'node-fetch';
+import { Handler } from "@netlify/functions";
+import sharp from "sharp";
+import fetch from "node-fetch";
 
 export const handler: Handler = async (event, context) => {
   try {
-    const body = JSON.parse(event.body ?? '');
-    
+    const body = JSON.parse(event.body ?? "");
+
     if (!body) {
       throw new Error("Invalid request");
     }
@@ -13,27 +13,32 @@ export const handler: Handler = async (event, context) => {
     const { width, height, x, y } = body;
 
     const imageUrl = `${process.env.URL}/paisagem.jpg`;
-    const image = await fetch(imageUrl).then(res => res.arrayBuffer());
+    const image = await fetch(imageUrl).then((res) => res.arrayBuffer());
 
     const result = await sharp(image)
       .png()
-      .extract({ width: width * 5, height: height * 5, top: Math.round(y * 5), left: Math.round(x * 5) })
+      .extract({
+        width: width * 5,
+        height: height * 5,
+        top: Math.round(y * 5),
+        left: Math.round(x * 5),
+      })
       .toBuffer();
 
     return {
       statusCode: 200,
-      body: result.toString('base64'),
+      body: result.toString("base64"),
       isBase64Encoded: true,
       headers: {
         "Content-Type": "image/png",
       },
-    }
+    };
   } catch (err) {
     return {
       statusCode: 500,
       body: JSON.stringify({
         message: err.message,
       }),
-    }
+    };
   }
-}
+};
